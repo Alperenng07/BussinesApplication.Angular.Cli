2059ae31c41dea029e654d9cfbdc90311f962710
Alperen TURKSOY <alperenguduk20@gmail.com>
2025-01-01 17:42:00 +0300

    API-ANGULAR BAGLANTISI YAPILMIŞ TASLAK PROJE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
+import { MatDialog } from '@angular/material/dialog';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'BussinesApplication.Angular.Cli';
+  title = 'MAHALLE ADİSYON.ng';
+  data: any;
+  public isButtonClicked1: boolean = false;
+  public isButtonClicked2: boolean = false;
+  public isButtonClicked3: boolean = false;
+  public isButtonClicked4: boolean = false;
+
+  toggleDivContent1() {
+    this.isButtonClicked1 = !this.isButtonClicked1;
+    this.isButtonClicked2 = false;
+    this.isButtonClicked3 = false;
+    this.isButtonClicked4 = false;
+  }
+
+  toggleDivContent2() {
+    this.isButtonClicked2 = !this.isButtonClicked2;
+    this.isButtonClicked1 = false;
+    this.isButtonClicked3 = false;
+    this.isButtonClicked4 = false;
+  }
+
+  toggleDivContent3() {
+    this.isButtonClicked3 = !this.isButtonClicked3;
+    this.isButtonClicked1 = false;
+    this.isButtonClicked2 = false;
+    this.isButtonClicked4 = false;
+  }
+  toggleDivContent4() {
+    this.isButtonClicked4 = !this.isButtonClicked4;
+    this.isButtonClicked3 = false;
+    this.isButtonClicked1 = false;
+    this.isButtonClicked2 = false;
+  }
+
+  constructor() {
+
+
+  }
+  openDialog() {
+   /* this.dialog.open(PlannedFormComponent);*/
+  }
+
+  ngOnInit(): void {
+
+  }
 }
